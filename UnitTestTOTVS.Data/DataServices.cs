4740145474d043eb96ac85cd4756c027483599ea
Breno Batista <brenojab@gmail.com>
2018-10-05 09:14:56 -0300

    Adding source

--- UnitTestTOTVS.Data/DataServices.cs
+++ UnitTestTOTVS.Data/DataServices.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using UnitTestTOTVS.Data.Interfaces;
 using UnitTestTOTVS.Data.Models;
 
 namespace UnitTestTOTVS.Data
---
+++ UnitTestTOTVS.Data/Interfaces/IDataServices.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnitTestTOTVS.Data.Models;
+
+namespace UnitTestTOTVS.Data.Interfaces
+{
+  public interface IDataServices
+  {
+    List<Esportista> GenerateListEsportistas();
+  }
+}
